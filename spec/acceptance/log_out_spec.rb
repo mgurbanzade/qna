7c9ce2ad3de5640e87d84b30e481261f409e25ba
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-04-22 20:45:31 +0400

    User can log out tests

---
+++ spec/acceptance/log_out_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User log out', %q{
+  As a logged user
+  I want to be able to log out
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Logged in user tries to log out' do
+    sign_in(user)
+    visit questions_path
+    click_on 'Log out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
