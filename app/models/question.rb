94c218eaed6f9fed56959bbd1d5f03114c4e94ae
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-05-24 17:15:13 +0400

    create supscription

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,7 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachable
+  has_many :subscriptions, dependent: :destroy
 
   validates :title, :body, presence: true
   scope :by_last, -> { order(created_at: :desc) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :answers
   has_many :comments
   has_many :authorizations, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
   def self.find_for_oauth(auth)
     authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
