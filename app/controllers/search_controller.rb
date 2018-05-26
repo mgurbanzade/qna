14b3118479977ac2e34f5a2f420072c1ed134020
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-05-26 16:43:43 +0400

    tests

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,6 @@ class SearchController < ApplicationController
   skip_authorization_check
 
   def index
-    @results = Search.query(params[:query], params[:condition])
+    @results = Search.query(params[:query], params[:resource])
   end
 end
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Search do
+  describe '.search' do
+    %w(Questions Answers Comments).each do |resource|
+      it "returns array of #{resource}" do
+        expect(ThinkingSphinx).to receive(:search).with('test search', classes: [resource.singularize.classify.constantize])
+        Search.query('test search', "#{resource}")
+      end
+    end
+
+    it "returns All types of resources" do
+      expect(ThinkingSphinx).to receive(:search).with('test search')
+      Search.query('test search', 'All')
+    end
+
+    it "Invalid condition" do
+      result = Search.query('test search', 'blah blah blah')
+      expect(result).to eql []
+    end
+  end
 end
