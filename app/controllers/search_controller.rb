1c891b35e65eba3af6bd03d0a90727bb0f8be1c1
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-05-30 16:55:27 +0400

    fix sphinx

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,6 @@ class SearchController < ApplicationController
   skip_authorization_check
 
   def index
-    @results = Search.query(params[:query], params[:resource])
+    @results = Search.query(params[:query], params[:condition])
   end
 end
