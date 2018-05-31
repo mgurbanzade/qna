8569284a50dd1b7d09f51d042e53029de5641d6a
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-05-31 17:15:48 +0400

    Deploy config

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.2"
+
+set :application, "qna"
+set :repo_url, "git@github.com:mgurbanzade/qna.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", ".env"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
