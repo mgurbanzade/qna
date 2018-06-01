92e6926f39109df70324195354e94b80e9ce7b2c
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-06-01 11:59:01 +0400

    Fix empty search query

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,6 +3,7 @@ class Search
 
   def self.query(query, resource)
     return [] unless RESOURCES.include?(resource)
+    return [] if query.empty?
     escaped_query = Riddle::Query.escape(query)
     return ThinkingSphinx.search escaped_query if resource == 'All'
     ThinkingSphinx.search escaped_query, classes: [resource.singularize.classify.constantize]
