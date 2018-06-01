5976afba2d422ea9bee79c36b61aa7da1b3897ef
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-06-01 18:38:16 +0400

    Module 22. Caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
+  #Enable/disable caching. By default caching is disabled.
+  #Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
@@ -21,8 +21,9 @@ Rails.application.configure do
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
+
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
 
     config.cache_store = :null_store
   end
